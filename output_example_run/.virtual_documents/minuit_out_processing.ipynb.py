d0e4dac0559b6df5fe53069a977ade7074d75ae9
AliAlkadhim <aa18dg@my.fsu.edu>
2021-07-23 14:30:54 -0400

    writing in RD

--- output_example_run/.virtual_documents/minuit_out_processing.ipynb.py
+++ output_example_run/.virtual_documents/minuit_out_processing.ipynb.py
@@ -163,6 +163,16 @@ for i in range(len(COV)):
 COV
 
 
+y = COV.view(type=np.matrix)
+def matprint(mat, fmt="g"):
+    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
+    for x in mat:
+        for i, y in enumerate(x):
+            print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end="  ")
+        print("")
+matprint(COV)
+
+
 COV.shape
 
 
